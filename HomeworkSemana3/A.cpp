c0e6fcfb935c7af8da97b1913c93bb6098d65d62
Sarah Melo <slm2@cin.ufpe.br>
2022-03-15 12:08:21 -0300

    Auto stash before merge of "main" and "origin/main"

--- HomeworkSemana3/A.cpp
+++ HomeworkSemana3/A.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int solve(vector <vector <int>> &g, vector <int> &v, int m, int catAlready, int pos, vector <bool> &vis){
+<<<<<<< Updated upstream
     vis[pos] = 1;
     if(catAlready>m) return 0;
     int folha = true;
@@ -21,6 +22,23 @@ int solve(vector <vector <int>> &g, vector <int> &v, int m, int catAlready, int 
         return 1;
     }
 
+=======
+    vis[pos] = true;
+    if(catAlready>m) return 0;
+    if(g[pos].empty()){
+        catAlready += v[pos];
+        //cout << "HERE: " << catAlready << " " << pos << "\n";
+        if(catAlready>m) return 0;
+        return 1;
+    }
+
+    int ans = 0;
+    for(auto i:g[pos]){
+        if(v[pos] == 1) ans += solve(g, v, m, catAlready+v[pos], i, vis);
+        else ans += solve(g, v, m, 0, i, vis);
+    }
+
+>>>>>>> Stashed changes
     return ans;
 }
 
@@ -31,7 +49,10 @@ int main(){
     int n, m;
     cin >> n >> m;
     vector <int> v(n);
+<<<<<<< Updated upstream
     vector <bool> vis(n, 0);
+=======
+>>>>>>> Stashed changes
     vector <vector <int>> g(n);
 
     for(int i=0; i<n; i++) cin >> v[i];
@@ -41,11 +62,28 @@ int main(){
         x--;
         y--;
         g[x].push_back(y);
+<<<<<<< Updated upstream
         g[y].push_back(x);
     }
 
     vis[0] = 1;
     cout << solve(g, v, m, v[0], 0, vis) << "\n";
+=======
+    }
+
+    int ans = 0;
+    for(int i=0; i<n; i++){
+        vector <bool> vis(n);
+        int temp = solve(g, v, m, 0, i, vis);
+        bool flag = true;
+        for(int j =0; j<n && flag == true; j++){
+            if(vis[j] == 0) flag = false;
+        }
+        if(flag == true) ans = max(temp, ans);
+    }
+
+    cout << ans << "\n";
+>>>>>>> Stashed changes
 
     return 0;
 }
